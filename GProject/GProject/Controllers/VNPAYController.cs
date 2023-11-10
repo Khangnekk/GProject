9b702c3bb740723a7f6ea4413942e828f226453b
Khangnekk <contact.khalgfk@gmail.com>
2023-11-10 07:04:50 +0700

    add project (80%)

---
+++ GProject/GProject/Controllers/VNPAYController.cs
@@ -0,0 +1,35 @@
+﻿using Gp.Services;
+using GProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GProject.Controllers
+{
+    public class VNPAYController : Controller
+    {
+        private readonly IVnPayService _vnPayService;
+
+        public VNPAYController(IVnPayService vnPayService)
+        {
+            _vnPayService = vnPayService;
+        }
+
+        [HttpPost(Name = "create-payment")]
+        public IActionResult CreatePaymentUrl(string OrderType, double Amount, string OrderDescription, string Name)
+        {
+            PaymentInformationModel model = new PaymentInformationModel();
+            model.OrderType = OrderType;
+            model.Amount = Amount;
+            model.OrderDescription = OrderDescription;
+            model.Name = Name;
+            return View(_vnPayService.CreatePaymentUrl(model, HttpContext));
+        }
+        [HttpGet]
+        public IActionResult PaymentCallback()
+        {
+            PaymentResponseModel response = (PaymentResponseModel)_vnPayService.PaymentExecute(Request.Query);
+            ViewBag.Response = response;
+            return View("/Views/Result.cshtml", response);
+        }
+    }
+
+}
---
+++ GProject/GProject/Program.cs
@@ -0,0 +1,39 @@
+using GProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddDistributedMemoryCache();
+        builder.Services.AddSession(options =>
+        {
+            options.IdleTimeout = TimeSpan.FromMinutes(30);
+            options.Cookie.HttpOnly = true;
+            options.Cookie.IsEssential = true;
+        });
+        var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        builder.Services.AddDbContext<NorthwindContext>(
+        options => options
+            .UseSqlServer(config.GetConnectionString("MyConnectionStrings")));
+        var app = builder.Build();
+
+        app.MapControllerRoute(
+        name: "default",
+        pattern: "/{controller}/{action}/"
+        );
+
+        app.MapControllerRoute(
+        name: "default",
+        pattern: "/{controller}/{action}/{id}"
+        );
+        app.MapControllerRoute(
+       name: "default",
+       pattern: "/{controller=Home}/{action=getAllProducts}"
+       );
+        app.UseSession();
+        app.Run();
+    }
+}
